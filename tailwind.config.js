369c5ea89ea87e896568688905470e5a08c8f056
schekur-com <export@schekur.com>
2026-06-16 10:06:11 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        schekur: {
+          navy: '#011627',  // Logonuzun o tok lacivert tonu
+          white: '#FFFFFF', // Logo yazı rengi
+          gray: '#F4F6F9',  // Teknik tablolar ve arka planlar için buzlu gri
+          steel: '#4A5568'  // İkincil yazılar ve ikonlar için çelik gri
+        }
+      }
+    }
+  }
+}
